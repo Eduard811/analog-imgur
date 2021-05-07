8af448629ba91c6992fe0c2620773768f8b7c072
Eduard811 <eduard.albertyan1@mail.ru>
2021-05-07 21:09:29 +0300

    likes, comments, views for ui

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -9,6 +9,7 @@ import { createPost } from '../../api/postAPI'
 import { Dialog, DialogContent, TextField, DialogActions } from '@material-ui/core'
 import { LOGIN_ROUTE } from '../../utils/consts'
 import Post from '../Post/Post'
+import Skeleton from '@material-ui/lab/Skeleton'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -30,10 +31,16 @@ const useStyles = makeStyles((theme: Theme) =>
       marginTop: 40,
       marginRight: -16,
       marginBottom: -16,
+      '@media (max-width: 567px)': {
+        marginRight: 0,
+      },
     },
     post: {
       marginRight: 16,
       marginBottom: 16,
+      '@media (max-width: 567px)': {
+        marginRight: 0,
+      },
     },
     addPost: {
       display: 'flex',
@@ -49,6 +56,14 @@ const useStyles = makeStyles((theme: Theme) =>
       },
       '@media (max-width: 859px)': {
         maxWidth: 536,
+      },
+    },
+    skeleton: {
+      width: 260,
+      height: 334,
+      borderRadius: 4,
+      '@media (max-width: 567px)': {
+        width: 'calc(100vw - 16px - 16px)',
       },
     },
   })
@@ -117,14 +132,24 @@ const Home: React.FC = () => {
             </Button>
           </div>
           {isLoading ? (
-            <h1>Идет загрузка...</h1>
+            <div className={classes.postsWrap}>
+              {Array(8)
+                .fill(0)
+                .map((_, index) => (
+                  <div className={classes.post} key={index}>
+                    <Skeleton animation="wave" variant="rect" className={classes.skeleton} />
+                  </div>
+                ))}
+            </div>
           ) : (
             <div className={classes.postsWrap}>
               {posts.map((post) => (
                 <div className={classes.post} key={post._id}>
                   <Post
+                    comments={post.comments}
+                    views={post.views}
+                    likes={post.likes}
                     title={post.title}
-                    description={post.description}
                     picture={process.env.REACT_APP_API_URL + post.picture}
                   />
                 </div>
--- client/src/components/Post/Post.tsx
+++ client/src/components/Post/Post.tsx
@@ -3,27 +3,49 @@ import Card from '@material-ui/core/Card'
 import CardActionArea from '@material-ui/core/CardActionArea'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
+import FavoriteIcon from '@material-ui/icons/Favorite'
+import SmsIcon from '@material-ui/icons/Sms'
+import VisibilityIcon from '@material-ui/icons/Visibility'
 import Typography from '@material-ui/core/Typography'
 
 const useStyles = makeStyles({
   root: {
     width: 260,
     '@media (max-width: 567px)': {
-      width: '100%',
+      width: 'calc(100vw - 16px - 16px)',
     },
   },
   media: {
     height: 260,
   },
+  icon: {
+    fontSize: 17,
+    marginRight: 3,
+  },
+  cardContent: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginTop: 5,
+  },
+  wrapIcon: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  counter: {
+    color: 'rgba(255, 255, 255, 0.3)',
+    fontSize: 13,
+  },
 })
 
 interface Props {
   title: string
-  description: string
   picture: string
+  likes: number
+  views: number
+  comments: any[]
 }
 
-const Post = ({ description, picture, title }: Props) => {
+const Post = ({ picture, title, likes, views, comments }: Props) => {
   const classes = useStyles()
 
   const i = picture.indexOf('.')
@@ -41,8 +63,28 @@ const Post = ({ description, picture, title }: Props) => {
         />
         <CardContent>
           <Typography variant="body2" color="textSecondary" component="p">
-            {description}
+            {title.length > 30 ? title.slice(0, 30) + ' ' + '...' : title}
           </Typography>
+          <div className={classes.cardContent}>
+            <div className={classes.wrapIcon}>
+              <FavoriteIcon color="disabled" className={classes.icon} />
+              <Typography className={classes.counter} component="p">
+                {likes}
+              </Typography>
+            </div>
+            <div className={classes.wrapIcon}>
+              <SmsIcon color="disabled" className={classes.icon} />
+              <Typography className={classes.counter} component="p">
+                {comments.length}
+              </Typography>
+            </div>
+            <div className={classes.wrapIcon}>
+              <VisibilityIcon color="disabled" className={classes.icon} />
+              <Typography className={classes.counter} component="p">
+                {views}
+              </Typography>
+            </div>
+          </div>
         </CardContent>
       </CardActionArea>
     </Card>
